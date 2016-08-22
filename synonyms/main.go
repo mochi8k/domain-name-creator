a1996c1708f2f570e3e7d6cd7ac4b75421aa8870
mochi8k <y.hkh.185@gmail.com>
2016-08-22 22:55:51 +0900

    add synnonyms

---
+++ synonyms/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/mochi8k/domain-name-generator/thesaurus"
+)
+
+func main() {
+
+	apiKey := os.Getenv("BHT_APIKEY")
+	thesaurus := &thesaurus.BigHuge{APIKey: apiKey}
+	s := bufio.NewScanner(os.Stdin)
+
+	for s.Scan() {
+		word := s.Text()
+		syns, err := thesaurus.Synonyms(word)
+		if err != nil {
+			log.Fatalf("bighuge: %qの類語検索に失敗しました: %v", word, err)
+		}
+
+		if len(syns) == 0 {
+			log.Fatalf("%qに類語はありませんでした\n", word)
+		}
+
+		for _, syn := range syns {
+			fmt.Println(syn)
+		}
+	}
+}
