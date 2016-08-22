1d63241effb3c9937ff0e7d39e38f2dd332cea3d
mochi8k <y.hkh.185@gmail.com>
2016-08-22 23:24:42 +0900

    fix path

--- thesaurus/bighuge.go
+++ thesaurus/bighuge.go
@@ -22,7 +22,7 @@ type words struct {
 func (b *BigHuge) Synonyms(term string) ([]string, error) {
 	var syns []string
 	response, err := http.Get(
-		"http://words.bighugelabs.com/api/2" + b.APIKey + "/" + term + "/json",
+		"http://words.bighugelabs.com/api/2/" + b.APIKey + "/" + term + "/json",
 	)
 	if err != nil {
 		return syns, fmt.Errorf("bighuge: %qの類語検索に失敗しました: %v", term, err)
@@ -33,7 +33,14 @@ func (b *BigHuge) Synonyms(term string) ([]string, error) {
 	if err := json.NewDecoder(response.Body).Decode(&data); err != nil {
 		return syns, err
 	}
-	syns = append(syns, data.Noun.Syn...)
-	syns = append(syns, data.Verb.Syn...)
+
+	if data.Noun != nil {
+		syns = append(syns, data.Noun.Syn...)
+	}
+
+	if data.Verb != nil {
+		syns = append(syns, data.Verb.Syn...)
+	}
+
 	return syns, nil
 }
