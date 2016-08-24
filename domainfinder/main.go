474e55bebf92274d547a8ea901409371f6db5c81
mochi8k <y.hkh.185@gmail.com>
2016-08-25 08:59:49 +0900

    add domainfinder

---
+++ domainfinder/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+)
+
+var cmdChain = []*exec.Cmd{
+	exec.Command("lib/synonyms"),
+	exec.Command("lib/sprinkle"),
+	exec.Command("lib/coolify"),
+	exec.Command("lib/domainify"),
+	exec.Command("lib/available"),
+}
+
+func main() {
+	// 標準入力
+	cmdChain[0].Stdin = os.Stdin
+
+	// 標準出力
+	cmdChain[len(cmdChain)-1].Stdout = os.Stdout
+
+	for i := 0; i < len(cmdChain)-1; i++ {
+		thisCmd := cmdChain[i]
+		nextCmd := cmdChain[i+1]
+		stdout, err := thisCmd.StdoutPipe()
+		if err != nil {
+			log.Panicln(err)
+		}
+		nextCmd.Stdin = stdout
+	}
+
+	for _, cmd := range cmdChain {
+		if err := cmd.Start(); err != nil {
+
+			// log.Fatallnだとdeferの処理はコールされない.
+			log.Panicln(err)
+		} else {
+			defer cmd.Process.Kill()
+		}
+	}
+
+	for _, cmd := range cmdChain {
+		if err := cmd.Wait(); err != nil {
+			log.Panicln(err)
+		}
+	}
+
+}
